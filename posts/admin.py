e0d1d56f1f3978bbb8c78396bf3b572a9ec29a81
Jann337 <145828833+Jann337@users.noreply.github.com>
2023-11-19 10:15:36 +0200

    admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Post
-# Register your models here.
+import djanngo.apps# Register your models here.
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'create_at', 'update_at', 'author')
+    # fields = ('title', 'body')
+    fieldsets = (
+        ('Post Information', {
+
+admin.site.register(Post, PostAdmin)
+
+class PostAdminSite(admin.AdminSite):
+    site_header = "MY:Admin Panel"
+
+
+post_site = PostAdminSite(name='Postadmin')
+post_site.register(Post, PostAdmin)
